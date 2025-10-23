67283c072a14f86f3c5c2e20a1eeefdcad151cc5
github.com (khaled abouseada) <khaled.a.seada@gmail.com>
2025-10-23 10:54:26 +0300

    Initial SwiftBootcampPro setup for 17-day bootcamp

---
+++ SwiftBootcampPro/SwiftBootcampPro/SwiftBootcampProApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBootcampProApp.swift
+//  SwiftBootcampPro
+//
+//  Created by Khaled on 23/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBootcampProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
